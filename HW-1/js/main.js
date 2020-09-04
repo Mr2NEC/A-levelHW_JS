4294e079e126dd0f5605da8731413feffc051965
Mr2NEC <vladislav.shpilka@gmail.com>
2020-09-04 20:28:43 +0300

    hw1js_start

---
+++ HW-1/js/main.js
@@ -0,0 +1,14 @@
+var Doctor1 = {
+    surname: "Kudryavtseva",
+    name: "Klava",
+    patronymic: "Nikolaevna",
+    age: 55,
+    specialization: "Therapist",
+};
+var Doctor2 = {
+    surname: "Kudryavtseva",
+    name: "Klava",
+    patronymic: "Nikolaevna",
+    age: 55,
+    specialization: "Therapist",
+};
