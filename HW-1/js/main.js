d7a99073f1f2d0a9a660c02d54156abff6a42245
Mr2nec <vladislav.shpilka@gmail.com>
2020-09-04 21:27:15 +0300

    commit

--- HW-1/js/main.js
+++ HW-1/js/main.js
@@ -1,14 +1,33 @@
-var Doctor1 = {
-    surname: "Kudryavtseva",
-    name: "Klava",
-    patronymic: "Nikolaevna",
-    age: 55,
-    specialization: "Therapist",
+var Patient = {
+    surname: "Burmistrov",
+    name: "Ivan",
+    patronymic: "Afonovich",
+    age: 27,
+    stomachache: true,
+    queuePosition: true,
 };
-var Doctor2 = {
-    surname: "Kudryavtseva",
-    name: "Klava",
-    patronymic: "Nikolaevna",
-    age: 55,
-    specialization: "Therapist",
+console.log(Patient);
+var Hospital = {
+    Doctor1: {
+        surname: "Kudryavtseva",
+        name: "Klava",
+        patronymic: "Nikolaevna",
+        age: 55,
+        specialization: "Therapist",
+        queuePerson: 25,
+        DPA: 50,
+        Patient: Patient,
+    },
+    Doctor2: {
+        surname: "Solovieva",
+        name: "Galina",
+        patronymic: "Anatolievna",
+        age: 47,
+        specialization: "Therapist",
+        queuePerson: 55,
+        DPA: 35,
+        Patient: Patient,
+    },
 };
+console.log(Hospital);
+console.log(Patient.queuePosition + Hospital.Doctor1.queuePerson);
