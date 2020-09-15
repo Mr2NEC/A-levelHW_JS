fcf3ca918b20674bb40cadde1b5a57533a77771e
Mr2nec <vladislav.shpilka@gmail.com>
2020-09-15 17:25:14 +0300

    hw3

---
+++ HW-3/js/main.js
@@ -0,0 +1,23 @@
+//Задание 1
+var persons = [
+    {
+        name: 'Ivan',
+        surname: 'Ivanovv',
+        fatherName: 'Ivanovich',
+    },
+];
+var person1 = {
+    name: 'Oleg',
+    esurname: 'Olegov',
+    fatherName: 'Olegovich',
+};
+var person2 = {
+    name: 'Nikolay',
+    surname: 'Nikolaev',
+    fatherName: 'Nikolaevich',
+};
+
+persons.push(person1, person2);
+console.log(persons);
+
+//Задание 2
