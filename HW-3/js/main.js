ac4b030e2f48e402155a95522f91210bdeb3d276
Mr2nec <vladislav.shpilka@gmail.com>
2020-09-21 16:32:31 +0300

    tasks_1-12

--- HW-3/js/main.js
+++ HW-3/js/main.js
@@ -62,58 +62,115 @@ function createArrPersons() {
     str += '</table>';
     document.write(str);
 }
-createArrPersons();
+createArrPersons(alert('tasks_1-7'));
 
 //Задание 8
-
-function avg2(a = 1, b = 1) {
-    return (a + b) / 2;
+function task8() {
+    function avg2(a = 1, b = 1) {
+        return (a + b) / 2;
+    }
+    console.log(avg2(5, 6));
 }
-console.log(avg2(5, 6));
+task8(alert('task_8'));
 
 //Задание 9
-
-function sum3(a = 0, b = 0, c = 0) {
-    return a + b + c;
+function task9() {
+    function sum3(a = 0, b = 0, c = 0) {
+        return a + b + c;
+    }
+    console.log(sum3(5, 6));
 }
-console.log(sum3(5, 6));
+task9(alert('task_9'));
 
 //Задание 10
-
-function enterNumber(text) {
-    var numberCustom = prompt(`Введите ${text} число `, '0');
-    number = parseInt(numberCustom);
-    return number;
-}
-
-function genRndInt(min, max) {
-    if (isNaN(min) && isNaN(max)) {
-        min = 0;
-        max = 1;
-    }
-    if (isNaN(max) || max === null) {
-        max = 0;
-    }
-    if (min > max) {
-        var c = min;
-        min = max;
-        max = c;
+function task10() {
+    function enterNumber(text) {
+        var numberCustom = prompt(`Введите ${text} число `, '0');
+        number = parseInt(numberCustom);
+        return number;
     }
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function intRandom() {
-    var minNumber, maxNumber;
-    do {
-        minNumber = enterNumber('минимальное');
-        maxNumber = enterNumber('максимальное');
-        if (minNumber > maxNumber) {
-            alert(`Максимальное число должно быть больше ${minNumber}!`);
+    function genRndInt(min, max) {
+        if (isNaN(min) && isNaN(max)) {
+            min = 0;
+            max = 1;
         }
-    } while (minNumber > maxNumber);
+        if (isNaN(max) || max === null) {
+            max = 0;
+        }
+        if (min > max) {
+            var c = min;
+            min = max;
+            max = c;
+        }
 
-    var rndInt = genRndInt(minNumber, maxNumber);
-    console.log(`Случайное число ${rndInt}`);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function intRandom() {
+        var minNumber, maxNumber;
+        do {
+            minNumber = enterNumber('минимальное');
+            maxNumber = enterNumber('максимальное');
+            if (minNumber > maxNumber) {
+                alert(`Максимальное число должно быть больше ${minNumber}!`);
+            }
+        } while (minNumber > maxNumber);
+
+        var rndInt = genRndInt(minNumber, maxNumber);
+        console.log(`Случайное число ${rndInt}`);
+    }
+    intRandom();
 }
-intRandom();
+task10(alert('task_10'));
+
+//Задание 11
+
+function task11() {
+    var arguments = {
+        a: 4,
+        b: 6,
+        c: 8,
+        d: 1,
+        f: 5,
+    };
+
+    function sum(arg) {
+        var sumArg = 0;
+        for (let i in arg) {
+            sumArg += arg[i];
+        }
+        console.log(sumArg);
+    }
+    sum(arguments);
+}
+task11(alert('task_11'));
+
+//Задание 12
+
+function task12() {
+    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    function splice(array, start, amount, ...insert) {
+        var arrReturn = [];
+        var arrMy = [...array];
+        array.length = 0;
+
+        for (let i = 0; i < start; i++) {
+            array.push(arrMy[i]);
+        }
+        for (let i = 0; i < insert.length; i++) {
+            array.push(insert[i]);
+        }
+        for (let i = start + amount; i < arrMy.length; i++) {
+            array.push(arrMy[i]);
+        }
+        for (let i = start; i < start + amount; i++) {
+            arrReturn.push(arrMy[i]);
+        }
+        return arrReturn;
+    }
+
+    console.log(splice(numbers, 3, 5, 97, 98, 99));
+    console.log(numbers);
+}
+task12(alert('task_12'));
