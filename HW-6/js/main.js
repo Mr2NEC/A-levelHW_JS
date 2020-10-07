4d680fada0d508f93b5cb64aa8cfe37345e3b7f2
Mr2nec <vladislav.shpilka@gmail.com>
2020-10-07 17:53:22 +0300

    commit

--- HW-6/js/main.js
+++ HW-6/js/main.js
@@ -1,16 +1,44 @@
-let getSomeInfo = function (type, index) {
-    if (isNaN(index)) {
-        index = '';
-    }
+let SomePosts = function getSomePosts() {
+    let url = 'https://jsonplaceholder.typicode.com/posts/';
+    return function (index) {
+        if (isNaN(index)) {
+            index = '';
+        }
 
-    fetch(`https://jsonplaceholder.typicode.com/${type}/${index}`)
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        fetch(url + index)
+            .then((response) => response.json())
+            .then((json) => createMassegeElem(json))
+            .then(() => createButtonElem());
+    };
 };
 
-getSomeInfo('posts', 1);
-getSomeInfo('comments', 1);
-getSomeInfo('albums', 1);
-getSomeInfo('photos', 1);
-getSomeInfo('todos', 1);
-getSomeInfo('users');
+SomePosts()(2);
+
+let wrapperElem = document.querySelector('#wrapper');
+
+function createMassegeElem(json) {
+    console.log(json);
+    let bodyMassege = document.createElement('div');
+    bodyMassege.style.cssText =
+        'width: 500px; padding: 10px; background: #00FFFF; display: flex; align-items: center; flex-direction: column; color: white';
+    wrapperElem.appendChild(bodyMassege);
+    let massegeTitle = document.createElement('div');
+    massegeTitle.style.cssText =
+        'width: 400px; border: 2px solid white; margin: 10px; padding: 10px; background: #00AAFF;';
+    massegeTitle.innerText = json.title;
+    bodyMassege.appendChild(massegeTitle);
+    let massegeBody = document.createElement('div');
+    massegeBody.style.cssText =
+        'width: 400px; border: 2px solid white; margin: 10px; padding: 10px; background: #00AAFF;';
+    massegeBody.innerText = json.body;
+    bodyMassege.appendChild(massegeBody);
+}
+
+function createButtonElem() {
+    let buttonElem = document.createElement('button');
+
+    buttonElem.style.cssText = 'width: 100px; height: 50px; margin-top: 20px;';
+    buttonElem.innerText = 'getData';
+
+    wrapperElem.appendChild(buttonElem);
+}
