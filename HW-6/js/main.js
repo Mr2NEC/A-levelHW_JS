6437b78b6f2ba8d5f9f99e812bf42be6155e1c38
Mr2nec <vladislav.shpilka@gmail.com>
2020-10-08 22:28:12 +0300

    commit

--- HW-6/js/main.js
+++ HW-6/js/main.js
@@ -1,101 +1,93 @@
-let wrapperElem = document.querySelector('#wrapper');
+let wrapperElem = document.querySelector("#wrapper");
 
-let bodyMassege = document.createElement('div');
+let bodyMassege = document.createElement("div");
 bodyMassege.style.cssText =
-    'width: 500px; padding: 10px; background: #00FFFF; display: flex; align-items: center; flex-direction: column; color: white';
-bodyMassege.id = 'bodyMassege';
+    "width: 500px; padding: 10px; background: #00FFFF; display: flex; align-items: center; flex-direction: column; color: white";
+bodyMassege.id = "bodyMassege";
 wrapperElem.appendChild(bodyMassege);
 
-let SomePosts = function getSomePosts() {
-    let url = 'https://jsonplaceholder.typicode.com/posts/';
-    return function (index) {
-        if (isNaN(index)) {
-            index = '';
-        }
+function createbuttonElem() {
+    let buttonElem = document.createElement("button");
 
-        fetch(url + index)
-            .then((response) => response.json())
-            .then((json) => getMassegeElem(json))
-            .then(() => somePost())
-            .then(() => somePut())
-            .then(() => somePatch());
-    };
+    buttonElem.style.cssText = "width: 100px; height: 50px; margin-top: 20px;";
+    buttonElem.innerText = "getData";
+
+    wrapperElem.appendChild(buttonElem);
+
+    buttonElem.addEventListener("click", () => {
+        Promise.all([someGet(), somePost(), somePut(), somePatch()]);
+    });
+}
+createbuttonElem();
+
+let someGet = function () {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => response.json())
+        .then((json) => getMassegeElem(json));
 };
-
-SomePosts()(1);
 
 function getMassegeElem(json) {
     console.log(json);
     for (i in json) {
-        if (i === 'title' || i === 'body') {
+        if (i === "title" || i === "body") {
             createElem(json[i]);
         }
     }
 }
 
 function createElem(text) {
-    let massege = document.createElement('div');
+    let massege = document.createElement("div");
     massege.style.cssText =
-        'width: 400px; border: 2px solid white; margin: 10px; padding: 10px; background: #00AAFF;';
+        "width: 400px; border: 2px solid white; margin: 10px; padding: 10px; background: #00AAFF;";
     massege.innerText = text;
     bodyMassege.appendChild(massege);
 }
 
-function createButtonElem() {
-    let buttonElem = document.createElement('button');
-
-    buttonElem.style.cssText = 'width: 100px; height: 50px; margin-top: 20px;';
-    buttonElem.innerText = 'getData';
-
-    wrapperElem.appendChild(buttonElem);
-}
-createButtonElem();
-
-function somePost() {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
+let somePost = function () {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
         body: JSON.stringify({
-            title: 'Lorem, ipsum dolor.',
+            title: "Lorem, ipsum dolor.",
             body:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, deserunt.',
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, deserunt.",
             userId: 1,
         }),
         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
+            "Content-type": "application/json; charset=UTF-8",
         },
     })
         .then((response) => response.json())
         .then((json) => console.log(json));
-}
+};
 
-function somePut() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PUT',
+let somePut = function () {
+    fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "PUT",
         body: JSON.stringify({
             id: 1,
-            title: 'Lorem ipsum dolor sit amet.',
+            title: "Lorem ipsum dolor sit amet.",
             body:
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Enim voluptatum alias facere eius, provident ipsum.',
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Enim voluptatum alias facere eius, provident ipsum.",
             userId: 1,
         }),
         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
+            "Content-type": "application/json; charset=UTF-8",
         },
     })
         .then((response) => response.json())
         .then((json) => console.log(json));
-}
+};
 
-function somePatch() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PATCH',
+let somePatch = function () {
+    fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "PATCH",
         body: JSON.stringify({
-            title: 'Lorem',
+            title: "Lorem",
         }),
         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
+            "Content-type": "application/json; charset=UTF-8",
         },
     })
         .then((response) => response.json())
         .then((json) => console.log(json));
-}
+};
