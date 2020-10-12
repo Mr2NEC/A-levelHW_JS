c5c8c7c260982286f8f1fb5e673a7629b792cdc9
Mr2nec <vladislav.shpilka@gmail.com>
2020-10-12 16:00:25 +0300

    HW6

--- HW-6/js/main.js
+++ HW-6/js/main.js
@@ -1,146 +1,182 @@
-let wrapperElem = document.querySelector("#wrapper");
+function HW_6() {
 
-let bodyMassege = document.createElement("div");
-bodyMassege.style.cssText =
-    "width: 500px; padding: 10px; background: #00FFFF; display: flex; align-items: center; flex-direction: column; color: white";
-wrapperElem.appendChild(bodyMassege);
+    let wrapperElem = document.querySelector("#wrapper");
 
-let timer = true;
+    let bodyMassege = document.createElement("div");
+    bodyMassege.style.cssText =
+        "width: 500px; padding: 10px; background: #00FFFF; display: flex; align-items: center; flex-direction: column; color: white";
+    wrapperElem.appendChild(bodyMassege);
 
-let counter = makeCounter();
+    let timer = true;
 
-function makeCounter () {
-    let i = 1;
-        return function () {
-            return i++;
-        };
-};
-function createSpanError (){
-    let spanElem = document.createElement("span");
-    spanElem.innerText = 'Chef, it is all gone !!!!';
-    bodyMassege.appendChild(spanElem);
-}
+    let counter = makeCounter();
 
-let someGet = function () {
-    let index = counter()
-    fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
-        .then((response) => response.json())
-        .then((json) => getMassegeElem(json))
+    function makeCounter () {
+        let i = 1;
+            return function () {
+                return i++;
+            };
+    };
+
+    function createSpanError (){
+        let spanElem = document.createElement("span");
+        spanElem.innerText = 'Chef, it is all gone !!!!';
+        bodyMassege.appendChild(spanElem);
+    }
+
+    // Task 2
+
+    let getPosts = function () {
+        let index = counter()
+        return () => fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
+            .then((response) =>        
+            {if(!response.ok){
+                // Task 6 
+                throw new Error('Не ОК')
+            }else {return response.json()}
+        })
+            .then((json) => getMassegeElem(json))
+            .catch(e => {
+                createSpanError(); 
+                getFailPosts(index);
+            });
+        };  
+
+    let postPosts = function () {
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify({
+                title: "Lorem, ipsum dolor.",
+                body:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, deserunt.",
+                userId: 1,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+            .catch((e) => {
+                console.log('Error: ' + e.message);
+            });
+    };
+    postPosts();
+
+    let putPosts = function () {
+        fetch("https://jsonplaceholder.typicode.com/posts/1", {
+            method: "PUT",
+            body: JSON.stringify({
+                id: 1,
+                title: "Lorem ipsum dolor sit amet.",
+                body:
+                    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Enim voluptatum alias facere eius, provident ipsum.",
+                userId: 1,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+            .catch((e) => {
+                console.log('Error: ' + e.message);
+                
+            });
+    };
+    putPosts();
+
+    let patchPosts = function () {
+        fetch("https://jsonplaceholder.typicode.com/posts/1", {
+            method: "PATCH",
+            body: JSON.stringify({
+                title: "Lorem",
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+            .catch((e) => {
+                console.log('Error: ' + e.message);   
+            });
+    };
+    patchPosts();
+
+    let deletePosts = function () {
+        fetch('https://jsonplaceholder.typicode.com/posts/100', {
+            method: 'DELETE',
+        })
         .catch((e) => {
-            createSpanError ()
             console.log('Error: ' + e.message);
-            console.log(e.reject);
         });
-};
+    }
+    deletePosts()
 
-function getMassegeElem(json) {
-    console.log(json);
-    for (i in json) {
-        if (i === "title" || i === "body") {
-            createElem(json[i]);
+    // Task 3
+
+    function getMassegeElem(json) {
+        console.log(json);
+        for (i in json) {
+            if (i === "title" || i === "body") {
+                createElem(json[i]);
+            }
         }
     }
-}
 
-function createElem(text) {
-    let massege = document.createElement("div");
-    massege.style.cssText =
-        "width: 400px; border: 2px solid white; margin: 10px; padding: 10px; background: #00AAFF;";
-    massege.innerText = text;
-    bodyMassege.appendChild(massege);
-}
+    function createElem(text) {
+        let massege = document.createElement("div");
+        massege.style.cssText =
+            "width: 400px; border: 2px solid white; margin: 10px; padding: 10px; background: #00AAFF;";
+        massege.innerText = text;
+        bodyMassege.appendChild(massege);
+    }
 
-let somePost = function () {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        body: JSON.stringify({
-            title: "Lorem, ipsum dolor.",
-            body:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, deserunt.",
-            userId: 1,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch((e) => {
-            console.log('Error: ' + e.message);
-            console.log(e.response);
+    // Task 4
+
+    function createbuttonElem() {
+        let buttonElem = document.createElement("button");
+        buttonElem.id = "btn"
+        buttonElem.style.cssText = "width: 100px; height: 50px; margin-top: 20px;";
+        buttonElem.innerText = "getData";
+
+        wrapperElem.appendChild(buttonElem);
+
+    // Task 5
+
+        buttonElem.addEventListener("click", () => {
+            if (timer === true) {
+                getPosts()()
+                timer = false
+                setTimeout( () => {
+                    timer = true
+                }, 3 * 1000)
+            }
         });
-};
-somePost();
+    }
+    createbuttonElem();
 
-let somePut = function () {
-    fetch("https://jsonplaceholder.typicode.com/posts/1", {
-        method: "PUT",
-        body: JSON.stringify({
-            id: 1,
-            title: "Lorem ipsum dolor sit amet.",
-            body:
-                "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Enim voluptatum alias facere eius, provident ipsum.",
-            userId: 1,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch((e) => {
-            console.log('Error: ' + e.message);
-            console.log(e.response);
-        });
-};
-somePut();
+    // Task 7
 
-let somePatch = function () {
-    fetch("https://jsonplaceholder.typicode.com/posts/1", {
-        method: "PATCH",
-        body: JSON.stringify({
-            title: "Lorem",
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch((e) => {
-            console.log('Error: ' + e.message);
-            console.log(e.response);
-        });
-};
-somePatch();
-
-let someDelete = function () {
-    fetch('https://jsonplaceholder.typicode.com/posts/100', {
-        method: 'DELETE',
-    })
-    .catch((e) => {
-        console.log('Error: ' + e.message);
-        console.log(e.response);
-    });
+    function getFailPosts(index) {
+        let button = document.querySelector('#btn')
+        button.setAttribute("disabled", "disabled");
+        function getFailPosts () {
+            fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
+            .then((response) => 
+            {if(!response.ok){
+                throw new Error('Не ОК')
+            }else {return response.json()}
+        })
+            .then((json) => { 
+                button.removeAttribute("disabled");
+                getMassegeElem(json);})
+            .catch(function () {
+                return setTimeout(getFailPosts, 10 * 1000)
+            } )  
+        }  
+        setTimeout(getFailPosts, 10 * 1000)
+    }
 }
-someDelete()
-
-function createbuttonElem() {
-    let buttonElem = document.createElement("button");
-
-    buttonElem.style.cssText = "width: 100px; height: 50px; margin-top: 20px;";
-    buttonElem.innerText = "getData";
-
-    wrapperElem.appendChild(buttonElem);
-
-    buttonElem.addEventListener("click", () => {
-        if (timer === true) {
-            someGet()
-            timer = false
-            setTimeout( () => {
-                timer = true
-            }, 3 * 1000)
-        }
-    });
-}
-createbuttonElem();
+HW_6()
