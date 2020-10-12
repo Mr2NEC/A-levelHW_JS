5a00d63ce95f659512c87843f9a077e6eadd2716
Mr2NEC <vladislav.shpilka@gmail.com>
2020-10-12 19:06:09 +0300

    commit

--- HW-6/js/main.js
+++ HW-6/js/main.js
@@ -1,5 +1,4 @@
 function HW_6() {
-
     let wrapperElem = document.querySelector("#wrapper");
 
     let bodyMassege = document.createElement("div");
@@ -11,36 +10,39 @@ function HW_6() {
 
     let counter = makeCounter();
 
-    function makeCounter () {
+    function makeCounter() {
         let i = 1;
-            return function () {
-                return i++;
-            };
-    };
+        return function () {
+            return i++;
+        };
+    }
 
-    function createSpanError (){
+    function createSpanError() {
         let spanElem = document.createElement("span");
-        spanElem.innerText = 'Chef, it is all gone !!!!';
+        spanElem.innerText = "Chef, it is all gone !!!!";
         bodyMassege.appendChild(spanElem);
     }
 
     // Task 2
 
     let getPosts = function () {
-        let index = counter()
-        return () => fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
-            .then((response) =>        
-            {if(!response.ok){
-                // Task 6 
-                throw new Error('Не ОК')
-            }else {return response.json()}
-        })
-            .then((json) => getMassegeElem(json))
-            .catch(e => {
-                createSpanError(); 
-                getFailPosts(index);
-            });
-        };  
+        let index = counter();
+        return () =>
+            fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
+                .then((response) => {
+                    if (!response.ok) {
+                        // Task 6
+                        throw new Error("Не ОК");
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then((json) => getMassegeElem(json))
+                .catch((e) => {
+                    createSpanError();
+                    getFailPosts(index);
+                });
+    };
 
     let postPosts = function () {
         fetch("https://jsonplaceholder.typicode.com/posts", {
@@ -58,7 +60,7 @@ function HW_6() {
             .then((response) => response.json())
             .then((json) => console.log(json))
             .catch((e) => {
-                console.log('Error: ' + e.message);
+                console.log("Error: " + e.message);
             });
     };
     postPosts();
@@ -80,8 +82,7 @@ function HW_6() {
             .then((response) => response.json())
             .then((json) => console.log(json))
             .catch((e) => {
-                console.log('Error: ' + e.message);
-                
+                console.log("Error: " + e.message);
             });
     };
     putPosts();
@@ -99,20 +100,19 @@ function HW_6() {
             .then((response) => response.json())
             .then((json) => console.log(json))
             .catch((e) => {
-                console.log('Error: ' + e.message);   
+                console.log("Error: " + e.message);
             });
     };
     patchPosts();
 
     let deletePosts = function () {
-        fetch('https://jsonplaceholder.typicode.com/posts/100', {
-            method: 'DELETE',
-        })
-        .catch((e) => {
-            console.log('Error: ' + e.message);
+        fetch("https://jsonplaceholder.typicode.com/posts/100", {
+            method: "DELETE",
+        }).catch((e) => {
+            console.log("Error: " + e.message);
         });
-    }
-    deletePosts()
+    };
+    deletePosts();
 
     // Task 3
 
@@ -137,21 +137,22 @@ function HW_6() {
 
     function createbuttonElem() {
         let buttonElem = document.createElement("button");
-        buttonElem.id = "btn"
-        buttonElem.style.cssText = "width: 100px; height: 50px; margin-top: 20px;";
+        buttonElem.id = "btn";
+        buttonElem.style.cssText =
+            "width: 100px; height: 50px; margin-top: 20px;";
         buttonElem.innerText = "getData";
 
         wrapperElem.appendChild(buttonElem);
 
-    // Task 5
+        // Task 5
 
         buttonElem.addEventListener("click", () => {
             if (timer === true) {
-                getPosts()()
-                timer = false
-                setTimeout( () => {
-                    timer = true
-                }, 3 * 1000)
+                getPosts()();
+                timer = false;
+                setTimeout(() => {
+                    timer = true;
+                }, 3 * 1000);
             }
         });
     }
@@ -160,23 +161,26 @@ function HW_6() {
     // Task 7
 
     function getFailPosts(index) {
-        let button = document.querySelector('#btn')
+        let button = document.querySelector("#btn");
         button.setAttribute("disabled", "disabled");
-        function getFailPosts () {
+        function getFailPosts() {
             fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
-            .then((response) => 
-            {if(!response.ok){
-                throw new Error('Не ОК')
-            }else {return response.json()}
-        })
-            .then((json) => { 
-                button.removeAttribute("disabled");
-                getMassegeElem(json);})
-            .catch(function () {
-                return setTimeout(getFailPosts, 10 * 1000)
-            } )  
-        }  
-        setTimeout(getFailPosts, 10 * 1000)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Не ОК");
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then((json) => {
+                    button.removeAttribute("disabled");
+                    getMassegeElem(json);
+                })
+                .catch(function () {
+                    return setTimeout(getFailPosts, 10 * 1000);
+                });
+        }
+        setTimeout(getFailPosts, 10 * 1000);
     }
 }
-HW_6()
+HW_6();
